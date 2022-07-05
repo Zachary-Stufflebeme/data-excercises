c9681bc2a712bf7fb4f9d24568cd21c8212eba07
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2022-07-05 15:17:45 -0500

    this is my work with tables in SQL

---
+++ Tables_excercises.sql
@@ -0,0 +1,13 @@
+#Use Employees Database
+USE employees;
+#List all tables in Database
+SHOW TABLES;
+#Explore employees table. data types include ints, dates, varchar's , enum
+DESCRIBE employees;
+#which tables contain. numeric type values? employee no.
+#Which tables contain String values? First name and last name.
+#Which tables contain a date type column? Birth date and Hire date
+#What is relationship between employees and dpartments tables? Emplyoees will belong to a department.
+# SHOW SQL that created dept_manager.
+SHOW CREATE TABLE dept_manager;
+
