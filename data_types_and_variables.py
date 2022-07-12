177e86c54763c8a2d5bddb0b029fab9e763dc1fa
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2022-07-12 15:24:40 -0500

    using python and vscode

---
+++ data_types_and_variables.py
@@ -0,0 +1,29 @@
+from operator import truediv
+
+
+print((3 * 3 ) + (5 * 3) + (3*1));
+
+#break
+
+print((400 * 6) + (380 * 4) + (350 * 10));
+
+#break
+full = False
+conflict = False
+if full == False and conflict == False:
+
+ print("enrolled")
+# break
+
+two_or_more = True
+expired = False
+if two_or_more == True and expired == False:
+    print("offer accepted")
+
+
+
+at_least_five = True
+no_more_twenty = True
+unique_combo = True
+if at_least_five == True and no_more_twenty == True and unique_combo == True:
+    print("account created")
